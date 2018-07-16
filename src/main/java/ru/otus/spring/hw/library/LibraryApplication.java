1741ae0e8a3ec7458e785b04655ea9ee49ad91eb
Denis <denkern@gmail.com>
2018-07-16 22:00:58 +0700

    issue #1

--- src/main/java/ru/otus/spring/hw/library/LibraryApplication.java
+++ src/main/java/ru/otus/spring/hw/library/LibraryApplication.java
@@ -3,10 +3,16 @@ package ru.otus.spring.hw.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.h2.tools.Console;
+
+import java.sql.SQLException;
+
+
 @SpringBootApplication
 public class LibraryApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Console.main(args);
         SpringApplication.run(LibraryApplication.class, args);
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into WRITERS (id, `name`) values (1, 'Dan Abnett');
+insert into BOOKS (id, `name`) values (1, 'PARIA');
+insert into GENRES (id, `name`) values (1, 'Warhammer 40000');
+
+insert into BOOK_GENRE (id, bookid, genreid) values (1, 1, 1);
+insert into BOOK_AUTHOR (id, bookid, authorid) values (1, 1, 1);
+
